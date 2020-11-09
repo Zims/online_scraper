383e976b6c86719b77ff4192eebcebfc328495fe
zims <zims@inbox.lv>
2020-11-09 11:41:14 +0200

    output folder/name/time added

--- server1.py
+++ server1.py
@@ -25,33 +25,13 @@ def result():
     # how to parse the flask dict:
     # https://stackoverflow.com/questions/23205577/python-flask-immutablemultidict
       the_url = request.form.getlist('Name')
+      file_name_chosen = request.form.getlist('Filename')
 
       print(the_url)
-      soup_yp.output_file(the_url[0])
+      print(file_name_chosen)
+      soup_yp.output_file(the_url[0], file_name_chosen[0])
+
       return render_template("result.html",result = result)
-
-
-# @app.route("/top_30", methods=['POST', 'GET'])
-# def top_30():
-#     return soup_yp.output_file()
-
-# @app.route("/files.html", methods=['GET'])
-# def files():
-#     return render_template('/files.html')
-
-# @app.route('/data/', methods = ['GET', 'POST'])
-# def data():
-#     if request.method == 'GET':
-#         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-#     if request.method == 'POST':
-#         form_data = request.form
-#         return render_template('data.html',form_data = form_data)
-
-
-# Start the server in terminal:
-
-#   python3 -m http.server
-# w/o pipenv but in the right directory. With index file in it
 
 #   gunicorn server1:app
 
--- soup_yp.py
+++ soup_yp.py
@@ -3,14 +3,18 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import sys
+from datetime import datetime
+
 # https://www.yellowpages.com/search?search_terms=hairstylist&geo_location_terms=New+York%2C+NY
 # https://www.yellowpages.com/search?search_terms=hairstylist&geo_location_terms=New+York%2C+FL
 # https://www.yellowpages.com/search?search_terms=makeup+artist&geo_location_terms=New+York%2C+FL
-
+file_name = ''
 
 def output_file(*args):
     def get_result():
         search_url = args[0]
+        global file_name
+        file_name = args[1]
         # search_url = str(input("Paste the Yellowpages address to scrape (copy the the address of Yellowpages search results): "))
         print(search_url)
         res = requests.get(search_url)
@@ -37,9 +41,12 @@ def output_file(*args):
 
     get_result()
     top_30 = list(get_result())
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
     print('DONE')
     # app_json = json.dumps(appDict)
-    with open('top_30.json', 'w') as json_file:
+    with open(f'output/{file_name}@{current_time}.json', 'w') as json_file:
         json.dump(top_30, json_file)
 
 # output_file(str('https://www.yellowpages.com/search?search_terms=makeup+artist&geo_location_terms=New+York%2C+FL'))
