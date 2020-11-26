8bbb64de4a2e1c276356089ef7d6dba248f4dffa
zims <zims@inbox.lv>
2020-11-26 19:29:53 +0200

    need to combine urls

--- soup_indeed.py
+++ soup_indeed.py
@@ -18,23 +18,27 @@ def output_file():
         soup = BeautifulSoup(res.text, 'html.parser')
 
         job_name_list = []
+        job_link_list = []
         company_name_list = []
+
         result_card = soup.select('.jobsearch-SerpJobCard.unifiedRow.result.row.unifiedRow')
         for job_offer in result_card:
             for job in job_offer.findAll("a", {"class": "jobtitle"}):
                 job_name = job.text
                 job_name_list.append(job_name.strip())
+            for job in job_offer.findAll("a", {"class": "jobtitle"}):
+                job_link = job.attrs['href']
+                job_link_list.append(job_link)
             for job in job_offer.findAll("span", {"class": "company"}):
                 company_name = job.text
                 company_name_list.append(company_name.strip())
-        
 
-        for i in job_name_list:
+        for i in job_link_list:
             print(i)
-        print(len(job_name_list))
-        for c in company_name_list:
-            print(c)
-        print(len(company_name_list))
+        # print(len(job_name_list))
+        # for c in company_name_list:
+        #     print(c)
+        # print(len(company_name_list))
         
     get_result()
         # for business in result_card:
